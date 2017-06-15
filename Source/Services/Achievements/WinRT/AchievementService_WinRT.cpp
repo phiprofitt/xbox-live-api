1e3fc4fe7fe0efd0bc30d8c5f364b14512af0701
phiprofitt <phipro@microsoft.com>
2017-06-14 17:42:02 -0700

    Revert "Changed achievementid to uint instead of string"
    This reverts commit e7b0bfa2384834ccbed82e80d1dc09bf93e9c5ea.

--- Source/Services/Achievements/WinRT/AchievementService_WinRT.cpp
+++ Source/Services/Achievements/WinRT/AchievementService_WinRT.cpp
@@ -25,13 +25,13 @@ AchievementService::AchievementService(
 Windows::Foundation::IAsyncAction^ 
 AchievementService::UpdateAchievementAsync(
     _In_ Platform::String^ xboxUserId,
-    _In_ uint32 achievementId,
+    _In_ Platform::String^ achievementId,
     _In_ uint32 percentComplete
     )
 {
     auto task = m_cppObj.update_achievement(
         STRING_T_FROM_PLATFORM_STRING(xboxUserId),
-        achievementId,
+        STRING_T_FROM_PLATFORM_STRING(achievementId),
         percentComplete)
     .then([](xbox_live_result<void> cppResult)
     {
@@ -46,7 +46,7 @@ AchievementService::UpdateAchievementAsync(
     _In_ Platform::String^ xboxUserId,
     _In_ uint32 titleId,
     _In_ Platform::String^ serviceConfigurationId,
-    _In_ uint32 achievementId,
+    _In_ Platform::String^ achievementId,
     _In_ uint32 percentComplete
     )
 {
@@ -54,7 +54,7 @@ AchievementService::UpdateAchievementAsync(
         STRING_T_FROM_PLATFORM_STRING(xboxUserId),
         titleId,
         STRING_T_FROM_PLATFORM_STRING(serviceConfigurationId),
-        achievementId,
+        STRING_T_FROM_PLATFORM_STRING(achievementId),
         percentComplete)
     .then([](xbox_live_result<void> cppResult)
     {
@@ -96,13 +96,13 @@ IAsyncOperation<Achievement^>^
 AchievementService::GetAchievementAsync(
     _In_ Platform::String^ xboxUserId,
     _In_ Platform::String^ serviceConfigurationId,
-    _In_ uint32 achievementId
+    _In_ Platform::String^ achievementId
     )
 {
     auto task = m_cppObj.get_achievement(
         STRING_T_FROM_PLATFORM_STRING(xboxUserId),
         STRING_T_FROM_PLATFORM_STRING(serviceConfigurationId),
-        achievementId)
+        STRING_T_FROM_PLATFORM_STRING(achievementId))
     .then([](xbox_live_result<achievement> cppResult)
     {
         THROW_IF_ERR(cppResult);
